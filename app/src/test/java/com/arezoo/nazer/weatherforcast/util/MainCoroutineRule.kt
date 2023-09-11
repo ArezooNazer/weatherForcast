f181037fc2befd9e79e06d5b00aead959eb14892
arezoo.nazerdeylami <arezoo.nazerdeylami@onefootball.com>
2023-09-11 18:07:48 +0200

    Add MainCoroutineRule

---
+++ app/src/test/java/com/arezoo/nazer/weatherforcast/util/MainCoroutineRule.kt
@@ -0,0 +1,28 @@
+package com.arezoo.nazer.weatherforcast.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * A junit Rule that sett Main Dispatcher,
+ */
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(
+    private val dispatcher: CoroutineDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
