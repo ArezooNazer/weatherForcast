b264dc235b8d6306816c1b13b6ab87db83fe13f4
arezoo.nazerdeylami <arezoo.nazerdeylami@onefootball.com>
2023-09-11 18:03:04 +0200

    Add location permission request

--- app/src/main/java/com/arezoo/nazer/weatherforcast/MainActivity.kt
+++ app/src/main/java/com/arezoo/nazer/weatherforcast/MainActivity.kt
@@ -3,6 +3,8 @@ package com.arezoo.nazer.weatherforcast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -15,9 +17,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.loadWeatherData()
+        }
+
+        permissionLauncher.launch(arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        ))
+
         setContent {
             WeatherForcastTheme {
                 val uiState by viewModel.weatherUiState.collectAsStateWithLifecycle()
